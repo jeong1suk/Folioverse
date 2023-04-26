8cccb20ecb721c1362b50252922a42dfac257d1d
Jay Jung <wjdwogns120523@gmail.com>
2023-04-26 10:54:52 +0900

    스켈레톤 파일 불러옴

---
+++ src/db/schemas/user.js
@@ -0,0 +1,34 @@
+const { Schema } = require ("mongoose");
+
+//UserSchema 정의하기
+const UserSchema = new Schema(
+  {
+    id: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: false,
+      default: "설명이 아직 없습니다. 추가해 주세요.",
+    },
+  },
+
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = UserSchema ;
