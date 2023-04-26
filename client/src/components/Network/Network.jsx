daa70eb3d066549c8c1f83d0bc76fb8c3405c8f0
Heyj <nb3065@gmail.com>
2023-04-26 20:18:52 +0900

    Feat: 유저데이터 more버튼을 누를 때 마다 50개씩 추가 조회기능 추가

--- client/src/components/Network/Network.jsx
+++ client/src/components/Network/Network.jsx
@@ -1,5 +1,6 @@
 // 정주현
 
+import { useEffect, useState } from "react";
 import { useAxiosGet } from "../../CustomHooks";
 import "./Network.css";
 import NetworkProfile from "./NetworkProfile";
@@ -7,6 +8,22 @@ const host = import.meta.env.VITE_SERVER_HOST;
 
 function Network() {
   const { data, error, loading } = useAxiosGet(`${host}/dummy/network`);
+  const [visibleData, setVisibleData] = useState([]);
+  const [listPrev, setListPrev] = useState(0);
+  const [listCur, setListCur] = useState(50);
+
+  useEffect(() => {
+    if (data) {
+      setVisibleData(data.slice(0, 50));
+    }
+  }, [data]);
+
+  useEffect(() => {
+    setVisibleData((prevVisibleData) => [
+      ...prevVisibleData,
+      ...data.slice(listPrev, listCur),
+    ]);
+  }, [data, listCur, listPrev]);
 
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error: {error.message}</div>;
@@ -14,7 +31,7 @@ function Network() {
   return (
     <div className='network-center-frame'>
       <div className='network-container'>
-        {data.map((user) => {
+        {visibleData.map((user) => {
           return (
             <NetworkProfile
               name={user.name}
@@ -24,6 +41,19 @@ function Network() {
             />
           );
         })}
+      </div>
+      <div className='moreData-wrapper'>
+        <button
+          className='moreData'
+          onClick={() => {
+            let curVal = listCur;
+            let prevVal = listPrev;
+            setListCur(curVal + 50);
+            setListPrev(prevVal + 50);
+          }}
+        >
+          More
+        </button>
       </div>
     </div>
   );
