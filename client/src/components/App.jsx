a16d38278842a50e4e3b87e47a08c007e9011a88
KubrickCode <billlee2656@gmail.com>
2023-04-26 23:33:53 +0900

    feat: 스토어,라우터,tailwind,다크모드 세팅
    zustand로 스토어 세팅을 하였고, 기본적인 라우터에 대한 세팅을 하였고, tailwind 세팅하였고(쓸사람만),
    스토어를 통해 localstorage로 다크모드를 구현하는 예시코드를 작성하였음.
    이부분에 대해서는 팀원들에게 충분히 설명할것.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,16 +1,9 @@
-import axios from "axios";
-
-const host = import.meta.env.VITE_SERVER_HOST;
+import Router from "../Router";
 
 const App = () => {
-  const handleClick = async () => {
-    const result = await axios.get(host + "/dummy/network");
-    console.log(result.data);
-  };
-
   return (
     <>
-      <button onClick={handleClick}>api 테스트 버튼</button>
+      <Router />
     </>
   );
 };
---
+++ client/src/components/UserSetting/UserSetting.jsx
@@ -0,0 +1,9 @@
+const UserSetting = () => {
+  return (
+    <>
+      <div>유저 세팅 페이지</div>
+    </>
+  );
+};
+
+export default UserSetting;
