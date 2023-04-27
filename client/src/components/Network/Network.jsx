b6f035044bcd93a5a85c0b47e7b7110e1457c383
Heyj <nb3065@gmail.com>
2023-04-27 22:14:44 +0900

    Fix: Network 컴포넌트가 비정상적으로 출력되는 현상 수정

--- client/src/components/Network/Network.jsx
+++ client/src/components/Network/Network.jsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { useAxiosGet } from "../../CustomHooks";
 import styles from "./Network.module.css";
 import NetworkProfile from "./NetworkProfile";
+import NetworkFilter from "./NetworkFilter";
 const host = import.meta.env.VITE_SERVER_HOST;
 
 function Network() {
@@ -25,35 +26,38 @@ function Network() {
   if (error) return <div>Error: {error.message}</div>;
 
   return (
-    <div className={styles.networkCenterFrame}>
-      <div className={styles.networkContainer}>
-        {visibleData.map((user, idx) => {
-          {
-            /* 라우터 설정 필요 */
-          }
-          return (
-            <NetworkProfile
-              name={user.name}
-              email={user.email}
-              description={user.description}
-              profileUrl={`/network/${user.id}`}
-              key={idx}
-            />
-          );
-        })}
+    <>
+      <div className={styles.networkCenterFrame}>
+        <NetworkFilter />
+        <div className={styles.networkContainer}>
+          {visibleData.map((user, idx) => {
+            {
+              /* 라우터 설정 필요 */
+            }
+            return (
+              <NetworkProfile
+                name={user.name}
+                email={user.email}
+                description={user.description}
+                profileUrl={`/network/${user.id}`}
+                key={idx}
+              />
+            );
+          })}
+        </div>
+        {complete && visibleData.length < data.length && (
+          <button
+            className={styles.moreData}
+            onClick={() => {
+              setListPrev(listCur);
+              setListCur(listCur + 30);
+            }}
+          >
+            More
+          </button>
+        )}
       </div>
-      {complete && visibleData.length < data.length && (
-        <button
-          className={styles.moreData}
-          onClick={() => {
-            setListPrev(listCur);
-            setListCur(listCur + 30);
-          }}
-        >
-          More
-        </button>
-      )}
-    </div>
+    </>
   );
 }
 
