87cc21782a8b76a9547db1307bb4bdd73b44ac56
wonsuk jeong <ssukgoon@gmail.com>
2023-04-27 15:14:00 +0900

    새로운 dev pull 하기 전 기존 App.jsx 커밋

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,18 +1,9 @@
-import axios from "axios";
-import LoginForm from "./components/sign/login";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-const host = import.meta.env.VITE_SERVER_HOST;
+import Router from "../Router";
 
 const App = () => {
-  const handleClick = async () => {
-    const result = await axios.get(host + "/dummy/network");
-    console.log(result.data);
-  };
-
   return (
     <>
-      <button onClick={handleClick}>api 테스트 버튼</button>
+      <Router />
     </>
   );
 };
