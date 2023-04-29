f989cf1cfbc17c41736b7ac8effa4b6ca65ac5f9
wonsuk jeong <ssukgoon@gmail.com>
2023-04-29 21:03:38 +0900

    Feat: 비밀번호 재설정 페이지, /login 경로에서 /ResetPassword경로로 이동

--- client/src/components/Login/ResetPassword.jsx
+++ client/src/components/Login/ResetPassword.jsx
@@ -1,6 +1,11 @@
-import React from "react";
+//정원석
+
+import React, { useState } from "react";
 import styles from "./ResetPassword.module.css";
+// 비밀번호 재설정할때 handlesubmit 함수 구현해야 함.
 function ResetPassword() {
+  const [password, setPassword] = useState("");
+
   return (
     <>
       <div className={styles.container}>
@@ -16,6 +21,10 @@ function ResetPassword() {
           </label>
         </div>
         <input className={styles.inputPwd} type="password" />
+
+        <button className={styles.btn} type="submit">
+          비밀번호 재설정
+        </button>
       </div>
     </>
   );
