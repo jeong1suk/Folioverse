1ae50d37b7d412b004cd6cdb2a5cbd47a02c1527
wonsuk jeong <ssukgoon@gmail.com>
2023-05-02 16:38:04 +0900

    로그인 tailwind css로 변경

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -46,43 +46,54 @@ function Login() {
 
   return (
     <>
-      <div className={styles.containerHead}>
+      <div
+        className={`max-w-2xl h-auto mx-auto p-10 mb-auto border border-solid border-slate rounded-5 shadow-sm flex justify-center iteims-center`}
+      >
+        {/* max-w-800px mx-auto p-20 border border-gray-300 rounded-lg shadow-sm flex justify-center items-center */}
         <img
-          className={styles.logo}
+          className={`w-20 h-auto mx-auto`}
           src="/logo/logo-light.png"
           alt="로고 이미지"
         />
-        <p className={styles.fvhead}>폴리오버스로 출발하기</p>
+        <p className={`mx-auto`}>폴리오버스로 출발하기</p>
       </div>
 
-      <div className={styles.container}>
-        <form className={styles.form}>
-          <label className={styles.label}>이메일</label>
+      <div
+        className={`max-w-2xl h-auto mx-auto p-10 mb-auto border border-solid border-slate rounded-5 shadow-sm`}
+      >
+        <form className={`flex flex-col`}>
+          <label className={`font-bold mr-5`}>이메일</label>
           <input
-            className={styles.inputTxt}
+            className={`p-2 border border-solid border-slate rounded`}
             type="email"
             placeholder="이메일"
             onChange={handleEmailChange}
           />
           <div>
-            <label htmlFor="password" className={styles.label}>
+            <label htmlFor="password" className={`font-bold mr-5`}>
               비밀번호:
             </label>
           </div>
           <input
-            className={styles.inputPwd}
+            className={`p-2 border border-solid border-slate rounded`}
             type="password"
             placeholder="비밀번호"
             onChange={handlePasswordChange}
           />
-          <br />
-          {errMessage && <div className={styles.inputErr}>{errMessage}</div>}
+
+          {errMessage && (
+            <div
+              className={`p-2 border border-solid border-slate rounded text-center bg-yellow-300 text-red-500`}
+            >
+              {errMessage}
+            </div>
+          )}
 
           <button
-            className={`${styles.btn} ${
+            className={`${`p-2 rounded-2xl border-none bg-black text-white cursor-pointer mt-2`} ${
               isEmailValid && isPasswordValid
-                ? styles.btnActive
-                : styles.btnDisabled
+                ? `hover: bg-blue-500 cursor-pointer`
+                : `cursor-wait`
             }`}
             type="submit"
             disabled={!isEmailValid || !isPasswordValid}
@@ -92,9 +103,11 @@ function Login() {
           </button>
         </form>
 
-        <div className={styles.alternativeLogin}>
-          <p className={styles.alternativeLoginP}>또는</p>
-          <button className={styles.btnGoogle}>
+        <div className={`mt-4 flex flex-col items-center mb-2`}>
+          <p className={`mb-2`}>또는</p>
+          <button
+            className={`bg-blue-500 text-white text-lg px-10 py-2 rounded-md shadow-md flex justify-center items-center hover:cursor-pointer`}
+          >
             <img
               className={styles.logo1}
               src="/logo/logo-light.png"
@@ -102,7 +115,9 @@ function Login() {
             />
             구글로 로그인
           </button>
-          <button className={styles.btnKakao}>
+          <button
+            className={`bg-yellow-300 text-white text-lg px-10 py-2 rounded-md shadow-md flex justify-center items-center hover:cursor-pointer`}
+          >
             <img
               className={styles.logo1}
               src="/logo/logo-light.png"
