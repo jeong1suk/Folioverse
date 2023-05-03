558352121b8292b90e507c9197de12dfd968897d
KubrickCode <billlee2656@gmail.com>
2023-05-03 17:42:09 +0900

    feat: 팔로우 프론트 구현 완료

--- client/src/components/UserPage/Profile.jsx
+++ client/src/components/UserPage/Profile.jsx
@@ -1,9 +1,13 @@
 //담당 : 이승현
 
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import useModalStore from "../../store/modalStore";
+import { useEffect } from "react";
+import { useQueryDelete, useQueryPatch } from "../../utils/useQuery";
+import { useQueryClient } from "react-query";
+import useToastStore from "../../store/toastStore";
 
-const Profile = ({ myInfo, othersInfo }) => {
+const Profile = ({ myInfo, othersInfo, followInfo }) => {
   const isToken = localStorage.getItem("token");
   const profileSrc = othersInfo
     ? othersInfo?.profile_image
@@ -51,7 +55,7 @@ const Profile = ({ myInfo, othersInfo }) => {
           othersInfo && isToken ? "block" : "hidden"
         } flex justify-center`}
       >
-        <ButtonGroup />
+        <ButtonGroup followInfo={followInfo} />
       </div>
     </aside>
   );
@@ -78,11 +82,47 @@ const MessageIcon = ({ id, name }) => {
   );
 };
 
-const ButtonGroup = () => {
+const ButtonGroup = ({ followInfo }) => {
+  const params = useParams();
+  const { mutate: doFollow } = useQueryPatch(`/follow/${params.id}`, "post");
+  const { deleteMutate: unFollow } = useQueryDelete("/follow");
+
+  const setToast = useToastStore((state) => state.setToast);
+
+  const queryClient = useQueryClient();
+
+  const handleFollow = () => {
+    if (followInfo) {
+      unFollow(params.id, {
+        onSuccess: () => {
+          queryClient.invalidateQueries("getFollowInfo");
+          setToast("팔로우를 취소하였습니다", "success");
+        },
+      });
+    } else {
+      doFollow(
+        {},
+        {
+          onSuccess: () => {
+            queryClient.invalidateQueries("getFollowInfo");
+            setToast("유저를 팔로우하였습니다", "success");
+          },
+        }
+      );
+    }
+  };
+
   return (
     <>
-      <button className="relative mx-5 inline-flex items-center justify-center p-0.5 mb-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-        <span className="relative flex px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+      <button
+        onClick={handleFollow}
+        className={`relative mx-5 inline-flex items-center justify-center p-0.5 mb-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800`}
+      >
+        <span
+          className={`relative flex px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 ${
+            followInfo && "dark:bg-gradient-to-br"
+          }`}
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
