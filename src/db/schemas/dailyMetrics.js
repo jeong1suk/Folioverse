4da2c19940b0e9eb402dd576cc539ff5931f9a6f
Jay Jung <wjdwogns120523@gmail.com>
2023-05-06 00:04:56 +0900

    comment:필요없는주석제거

--- src/db/schemas/dailyMetrics.js
+++ src/db/schemas/dailyMetrics.js
@@ -3,12 +3,11 @@ import { Schema, model } from "mongoose";
 const date = new Date();
 date.setHours(date.getHours() + 9);
 
-//   *** index 공부
+
 const dailySchema = new Schema({
   date: {
     type: String,
     default: `${date.toISOString().substr(0, 10)}`,
-    //  2023-05-02
   },
   user_id: { type: Schema.Types.ObjectId, ref: "User", required: true },
   follow_count: { type: Number, default: 0 },
